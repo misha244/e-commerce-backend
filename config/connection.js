824c6767b86eff513c7dd2e2fcc89eb3d8f7c58c
Misha <73886737+misha244@users.noreply.github.com>
2021-06-12 13:29:41 +0100

    refactor connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,17 @@
-require("dotenv").config();
-
-const Sequelize = require("sequelize");
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
-
-module.exports = sequelize;
+const sequelize = require("sequelize");
+const dbOptions = {
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+  port: process.env.DB_PORT,
+  logging: false,
+};
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = new sequelize(process.env.JAWSDB_URL);
+} else {
+  connection = new sequelize(dbName, dbUser, dbPassword, dbOptions);
+}
+module.exports = connection;
